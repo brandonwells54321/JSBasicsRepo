27640957979bea865cb896ec0e7f6f287fe52f8c
Brandon Wells <111785921+brandonwells54321@users.noreply.github.com>
2025-04-04 17:16:30 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,7 @@ var button = document.querySelector('button');
 var box = document.getElementById('changeMe');
 
 function changeColor(){
-    if(box.style.backround == 'green'){
+    if(box.style.background == 'green'){
         box.style.background = 'blue';
     }else{
         box.style.background = 'green';
